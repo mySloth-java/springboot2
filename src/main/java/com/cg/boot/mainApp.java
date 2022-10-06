919f505e701671fa528c2f1860be7f94f0bffe81
zero <cg2100773258@163.com>
2022-10-06 21:19:37 +0800

    springboot后台管理登录v2.0

---
+++ src/main/java/com/cg/boot/mainApp.java
@@ -0,0 +1,12 @@
+package com.cg.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class mainApp {
+    public static void main(String[] args) {
+        //执行主程序
+        SpringApplication.run(mainApp.class,args);
+    }
+}
